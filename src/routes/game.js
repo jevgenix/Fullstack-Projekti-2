07f0bdf96a321d8e74fdc7fbac4c029ba53b3e03
jevgenix <71660541+jevgenix@users.noreply.github.com>
2022-04-08 01:31:51 +0300

    some logic, set Interval, API first usage

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,9 +1,30 @@
 // express added
 const express = require("express");
 const gameRouter = express.Router();
+const axios = require("axios");
 
-gameRouter.get("/", (req, res) => {
-  res.render("pages/index");
+gameRouter.get("/", async (req, res) => {
+  // res.render("pages/index");
+  try {
+    const gameAPI = await axios.get(
+      "https://bad-api-assignment.reaktor.com/rps/history"
+    );
+
+    const key = gameAPI.data.cursor;
+    const data = gameAPI.data.data;
+
+    res.render("pages/index");
+  } catch (err) {
+    if (err.response) {
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    } else if (err.request) {
+      console.log(err.request);
+    } else {
+      console.error("Error", err.message);
+    }
+  }
 });
 
 module.exports = gameRouter;

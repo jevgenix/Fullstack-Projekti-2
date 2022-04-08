6118283eadd837a7ee2a5787b68508897441f521
jevgenix <71660541+jevgenix@users.noreply.github.com>
2022-04-08 22:25:21 +0300

    live game session

---
+++ game.js
@@ -0,0 +1,80 @@
+// import fetch to our frontend.js
+// deleted from package.json "type": "module",
+
+// setting api-url
+const API_URL = "https://bad-api-assignment.reaktor.com/rps/history";
+
+let game = document.getElementById("game-results");
+
+// images to add!
+const rock_img = document.createElement("img");
+rock_img.src = "../img/rock.PNG";
+
+const paper_img = document.createElement("img");
+paper_img.src = "../img/paper.PNG";
+
+const scissors_img = document.createElement("img");
+scissors_img.src = "../img/scissors.PNG";
+
+// rock paper scissors function which outputs the winner
+function winner(playerA, playerA_hand, playerB, playerB_hand) {
+  const r = "ROCK";
+  const p = "PAPER";
+  const s = "SCISSORS";
+
+  if (playerA_hand == playerB_hand) {
+    return "Tasapeli!";
+  }
+
+  if (playerA_hand == r) {
+    if (playerB_hand == s) {
+      return playerA;
+    } else {
+      return playerB;
+    }
+  }
+
+  if (playerA_hand == p) {
+    if (playerB_hand == r) {
+      return playerA;
+    } else {
+      return playerB;
+    }
+  }
+
+  if (playerA_hand == s) {
+    if (playerB_hand == p) {
+      return playerA;
+    } else {
+      return playerB;
+    }
+  }
+}
+
+getSession(API_URL);
+
+function getSession(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const progress = data.data;
+
+      console.log(progress.length - 1);
+
+      // counter for our interval
+      //let counter = 0;
+      //const i = setInterval(() => {
+      //  let result = winner(
+      //    progress[counter].playerA.name,
+      //    progress[counter].playerA.played,
+      //    progress[counter].playerB.name,
+      //    progress[counter].playerB.played
+      //  );
+      //  counter++;
+      //  console.log(result);
+      //  if (counter == progress.length - 1) {
+      //    clearInterval(i);
+      //  }
+      //}, 1000);
+    });
+}
--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use static content from public dir
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/img", express.static(__dirname + "/img"));
+app.use("/js", express.static(__dirname + "/js"));
 
 // set up ejs engine
 app.set("views", "./src/views");
